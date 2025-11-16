41d0f67d2039f2b63398cf4dc4e6769229e937c1
unknown <rudolfschulthess02@gmail.com>
2025-11-16 13:41:16 +0100

    include structure angepasst

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-include 'header.php';
+include 'include/header.php';
 ?>
 <body class="d-flex flex-column min-vh-100">
     <main>
 
 <?php
-include 'footer.php';
+include 'include/footer.php';
 ?>
