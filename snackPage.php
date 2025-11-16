8c1e0d067b3f653d2566e7905b0b2a22aab0dcf4
unknown <rudolfschulthess02@gmail.com>
2025-11-16 16:09:10 +0100

    Snack Seite angefangen

---
+++ snackPage.php
@@ -0,0 +1,28 @@
+<?php
+include 'include/header.php';
+?>
+
+<body class="d-flex flex-column">
+    <div class="container-fluid row my-5">
+        <div class="col">
+            <img src="data/snackImages/Doritos-Tortilla-Chips-Nacho-Cheese-Flavored-Snack-Chips-2-75-oz-Bag_c6a6c39d-c8b5-4478-beba-35e7f14e8171.26c8df2fae95aeb86bc07a3c81489399.avif" class="card-img-top img-fluid" alt="...">
+        </div>
+        <div class="col">
+            <h1>Nacho Cheese</h1>
+            <h2>Dorritos</h2>
+
+            <div class="d-flex space my-5">
+            <a href="#" class="btn btn-lg btn-primary me-4">Write Review</a>
+            <a href="#" class="btn btn-lg btn-primary">Eaten +1</a>
+            </div>
+        </div>
+
+        <div class="d-flex col justify-content-start">
+        <img src="data/images/star.png" 
+            class=""
+            style="height:60px; object-fit:contain;" 
+            alt="Star">
+        <h1 class="card-text ">8</h1>
+    </div>
+    </div>
+    </div>
