3900efabacf5835a67594c835ec610d0bd330493
unknown <rudolfschulthess02@gmail.com>
2025-11-18 18:18:37 +0100

    Bilder bei karten können jetzt auch gecklickt werden

--- include/snackCardShowAll.php
+++ include/snackCardShowAll.php
@@ -1,6 +1,7 @@
 <div class="card m-1 py-2" style="width: 18rem; height: 350px;">
-  <img src="/Webdesign-adrian-rudi/data/images/showAll.png" class="card-img-top img-fluid" style="height:150px; object-fit:contain;" alt="...">
-
+  <a href="/Webdesign-adrian-rudi/allSnacks.php">
+    <img src="/Webdesign-adrian-rudi/data/images/showAll.png" class="card-img-top img-fluid" style="height:150px; object-fit:contain;" alt="...">
+  </a>
   <div class="d-flex justify-content-center align-items-center mt-5">
     <a href="/Webdesign-adrian-rudi/allSnacks.php" class="btn btn-lg btn-primary">More</a>
   </div>
