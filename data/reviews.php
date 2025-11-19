7ab1daa72bd6ff3b76aeecedb103bfa791557cae
Rudi <rudolfschulthess02@gmail.com>
2025-11-19 16:35:49 +0100

    snacks können nur hinzugefügt werden wenn angemeldet

--- data/reviews.php
+++ data/reviews.php
@@ -2,7 +2,7 @@
 return array (
   0 => 
   array (
-    'snackID' => 1,
+    'snackID' => -1,
     'user' => 'admin',
     'title' => 'Top preview example',
     'text' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt eos, explicabo molestias expedita eum inventore nulla quidem id, labore laudantium accusantium. Voluptatum error quo porro obcaecati inventore magni necessitatibus in.',
--- include/snackCardAddNew.php
+++ include/snackCardAddNew.php
@@ -2,7 +2,16 @@
   <a href="/Webdesign-adrian-rudi/addSnack.php">
     <img src="/Webdesign-adrian-rudi/data/images/addMore.png" class="card-img-top img-fluid" style="height:150px; object-fit:contain;" alt="...">
   </a>
-  <div class="d-flex justify-content-center align-items-center mt-5">
-    <a href="/Webdesign-adrian-rudi/addSnack.php" class="btn btn-lg btn-primary">Add new</a>
-  </div>
+  <?php if (isset($_SESSION['user'])): ?>
+      <div class="d-flex justify-content-center mt-5">
+          <a href="/Webdesign-adrian-rudi/addSnack.php" class="btn btn-lg btn-primary">Add new</a>
+      </div>
+      <?php else: ?>
+        <div class="d-flex flex-column justify-content-center align-items-center mt-5">
+            <a href="/Webdesign-adrian-rudi/addSnack.php" class="btn btn-lg btn-secondary disabled mb-2">
+                Add new
+            </a>
+            <p class="fw-bold text-center mx-2">Login or create account to add new snack</p>
+        </div>
+      <?php endif; ?>
 </div>
