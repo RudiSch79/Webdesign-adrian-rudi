83099399448dcb89e2102cfbd7b0db1e2452bfee
unknown <rudolfschulthess02@gmail.com>
2025-11-19 18:48:39 +0100

    kleiner bugfix

--- include/snackCardAddNew.php
+++ include/snackCardAddNew.php
@@ -1,12 +1,14 @@
 <div class="card m-1 py-2" style="width: 18rem; height: 350px;">
-  <a href="/Webdesign-adrian-rudi/addSnack.php">
-    <img src="/Webdesign-adrian-rudi/data/images/addMore.png" class="card-img-top img-fluid" style="height:150px; object-fit:contain;" alt="...">
-  </a>
+
   <?php if (isset($_SESSION['user'])): ?>
+      <a href="/Webdesign-adrian-rudi/addSnack.php">
+          <img src="/Webdesign-adrian-rudi/data/images/addMore.png" class="card-img-top img-fluid" style="height:150px; object-fit:contain;" alt="...">
+      </a>
       <div class="d-flex justify-content-center mt-5">
           <a href="/Webdesign-adrian-rudi/addSnack.php" class="btn btn-lg btn-primary">Add new</a>
       </div>
       <?php else: ?>
+        <img src="/Webdesign-adrian-rudi/data/images/addMore.png" class="card-img-top img-fluid" style="height:150px; object-fit:contain;" alt="...">
         <div class="d-flex flex-column justify-content-center align-items-center mt-5">
             <a href="/Webdesign-adrian-rudi/addSnack.php" class="btn btn-lg btn-secondary disabled mb-2">
                 Add new
