29ecdcb06ea31778427332bc9a371bbca5f51388
unknown <rudolfschulthess02@gmail.com>
2026-01-21 14:36:04 +0100

    Filter backend

--- snacks.php
+++ snacks.php
@@ -5,7 +5,10 @@ include "include/errorSucessPopups.php";
 
 $pdo = db();
 
-$stmt = $pdo->query("
+$brandID = isset($_GET['brand']) ? (int)$_GET['brand'] : null;
+$categorieID = isset($_GET['categorie']) ? (int)$_GET['categorie'] : null;
+
+$sql = "
     SELECT
         s.id,
         s.name,
@@ -15,13 +18,27 @@ $stmt = $pdo->query("
         COUNT(r.id) AS review_count
     FROM snacks s
     JOIN brands b ON b.id = s.brand_id
+    Join categories c ON c.id = s.categorie_id
     LEFT JOIN reviews r ON r.snack_id = s.id
+";
+
+$params = [];
+if ($brandID !== null) {
+    $sql .= " WHERE b.id = :brandID";
+    $params[':brandID'] = $brandID;
+}
+if ($categorieID !== null) {
+    $sql .= " WHERE c.id = :categorieID";
+    $params[':categorieID'] = $categorieID;
+}
+
+$sql .= "
     GROUP BY s.id, s.name, s.image_path, b.name
     ORDER BY s.created_at DESC
-");
-$rows = $stmt->fetchAll();
-
-
+";
+$stmt = $pdo->prepare($sql);
+$stmt->execute($params);
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <body class="d-flex flex-column min-vh-100">
@@ -31,7 +48,7 @@ $rows = $stmt->fetchAll();
         <?php
         foreach ($rows as $row) {
             $snack = [
-                'id'     => (int)$row['id'],
+                'id'     => $row['id'],
                 'name'   => $row['name'],
                 'brand'  => $row['brand_name'],
                 'rating' => (float)$row['avg_rating'],
